c617fd8d2458df940e14a108b17df507bfebcc02
Maria <mmamonova95@gmail.com>
2016-11-13 18:45:59 +0300

    added module fixed config cities and students was inserted in DB

--- app.rb
+++ app.rb
@@ -4,11 +4,43 @@ require "hash_dot"
 require "sqlite3"
 require "yaml"
 
+require_relative "modules/inser"
+
 @config = YAML.load_file("config.yml").to_dot
+
 
 DB = Sequel.connect(
     :adapter => 'mysql2',
     :host => @config.db_host,
+    :database => @config.db_name,
     :user => @config.db_user,
     :password => @config.db_password)
 )
+
+
+if DB.tables.include? :cities
+    DB.drop_table :cities
+
+DB.create_table :cities do
+  primary_key :id
+  String :city_name
+  String :city_country
+end
+
+if DB.tables.include? :students
+    DB.drop_table :students
+
+DB.create_table :students do
+    primary_key :student_id
+    String :name
+    String :surname
+    Integer :course
+    TrueClass :is_male
+    foreign_key :city_id
+end
+
+cities = DB[:cities]
+students = DB[:students]
+
+insert_cities
+insert_students
